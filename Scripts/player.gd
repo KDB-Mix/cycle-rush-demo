aa3b86dfca92510cc29555516d969f2cf5530c68
KDB-Mix <kareemdb3+github@gmail.com>
2026-05-21 13:05:31 +0400

    fixed some things

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -2,22 +2,13 @@ extends Camera3D
 @onready var animated_sprite_3d: AnimatedSprite3D = $AnimatedSprite3D
 
 func _physics_process(delta: float) -> void:
-	if Input.is_action_pressed("ui_right") and global_position.x < 1.15:
-		position.x += delta
+	if Input.is_action_pressed("ui_right") and global_position.x < 1.14:
+		global_position.x += delta*3
 		animated_sprite_3d.play("right")
-	elif position.x > 1.15:
-		global_position.x = 1.15
-	elif Input.is_action_pressed("ui_left") and global_position.x > -1.15:
-		position.x -= delta
+	elif Input.is_action_pressed("ui_left") and global_position.x > -1.14:
+		global_position.x -= delta*3
 		animated_sprite_3d.play("left")
-	elif position.x < -1.15:
-		global_position.x = -1.15
 	else:
 		animated_sprite_3d.play("strait")
 	print(global_position)
-	elif Input.is_action_pressed("ui_left") and global_position.x > -1.15:
-		position.x -= delta
-		animated_sprite_3d.play("left")
-	else:
-		animated_sprite_3d.play("strait")
-	position.x = clamp(position.x, -1.15, 1.15)
+	global_position.x = clamp(global_position.x, -1.15, 1.15)
