aa669166f62577270ddeda111fd6b1ee905d6fd9
alfathmtsm220-hub <alfathmtsm220@gmail.com>
2026-05-21 07:48:16 +0400

    Update player.gd

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -5,12 +5,9 @@ func _process(delta: float) -> void:
 	if Input.is_action_pressed("ui_right") and global_position.x < 1.15:
 		position.x += delta
 		animated_sprite_3d.play("right")
-	elif position.x >= 1.15:
-		global_position.x = 1.15
 	elif Input.is_action_pressed("ui_left") and global_position.x > -1.15:
 		position.x -= delta
 		animated_sprite_3d.play("left")
-	elif position.x <= -1.15:
-		global_position.x = -1.15
 	else:
 		animated_sprite_3d.play("strait")
+	position.x = clamp(position.x, -1.15, 1.15)
