b5d26210b23c21f315a9018dfce93c9bad47a596
KDB-Mix <kareemdb3+github@gmail.com>
2026-05-30 23:03:55 +0400

    ok this is the last commit before first demo test release

--- Scripts/main_level.gd
+++ Scripts/main_level.gd
@@ -14,8 +14,18 @@ func spawnRock(rockScene):
 	var time = 1
 	var timer = get_tree().create_timer(time)
 	await timer.timeout
-	var rockNode :Node3D = rockScene.instantiate()
-	rocks.add_child(rockNode)
-	rockNode.global_position = Vector3(randf()*2-1, .555, player.global_position.z-2)
+	var rockpos: Vector3 = Vector3.ZERO
+	for i in range(2):
+		var rockNode :Rock = rockScene.instantiate()
+		rocks.add_child(rockNode)
+		rockNode.global_position = Vector3(randf()*1.8-.9, .555, player.global_position.z-3)
+		if rockpos != Vector3.ZERO:
+			fixOverlap(rockNode, rockpos, .42)
+		rockpos = rockNode.global_position
 	spawnRock(rockScene)
+
+func fixOverlap(rockNode, rockpos, range):
+	if rockNode.global_position.x > rockpos.x-range and rockNode.global_position.x < rockpos.x+range:
+		rockNode.global_position = Vector3(randf()*1.8-.9, .555, player.global_position.z-3)
+		fixOverlap(rockNode ,rockpos, range)
 	
---
+++ Scripts/touch.gd
@@ -0,0 +1,41 @@
+class_name TouchScreen
+extends CanvasLayer
+
+@onready var left: TouchScreenButton = $Left
+@onready var right: TouchScreenButton = $Right
+@onready var control: Control = $Control
+
+
+var last_screen_size := Vector2.ZERO
+
+func _ready():
+	update_buttons()
+
+func _process(_delta):
+	var current_size = control.get_viewport_rect().size
+	if current_size != last_screen_size:
+		last_screen_size = current_size
+		update_buttons()
+
+func update_buttons():
+	if not left or not right or not control:
+		return
+
+	var screen_size = control.get_viewport_rect().size
+	var half_width = screen_size.x / 2
+	var full_height = screen_size.y
+
+	var tex_size = left.texture_normal.get_size()
+	if tex_size.x == 0 or tex_size.y == 0:
+		return
+
+	var button_scale = Vector2(
+		half_width / tex_size.x,
+		full_height / tex_size.y
+	)
+
+	left.scale = button_scale
+	left.position = Vector2(0, 80)
+
+	right.scale = button_scale
+	right.position = Vector2(half_width, 80)
